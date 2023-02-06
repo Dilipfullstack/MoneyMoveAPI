ce4030bc22faec943285d1ab2c113e7bbd3b351c
Abhishek Oza <101032683+Ahishekoza@users.noreply.github.com>
2023-02-06 20:35:47 +0530

    'LoginPage'

--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from '@/plugins/vuetify'
 
-createApp(App).use(store).use(router).mount('#app')
+import '@fortawesome/fontawesome-free/css/all.css'
+
+createApp(App).use(store).use(Vuetify).use(router).mount('#app')
